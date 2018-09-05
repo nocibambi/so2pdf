b47198d12015c9779c0ea85eaea761315e52da28
nocibambi <nocibambi@gmail.com>
2018-09-05 23:53:37 +0200

    documentation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # so2pdf
-Script to create pdf from stack exchange posts based on their codes and through http://stackprinter.com.
+Script to create pdf from stackoverflow posts based on their codes. It works by calling the API of http://stackprinter.com which renders a print-friendly form of the post. This is useful if you like to read the posts offline or you want to archive them.
+
+Currently it does not support the other stack excange pages.
+
+## Required libraries
+- urllib
+- bs4
+- weasyprint
+- re
+
+## Usage
+1. Get the codes of the posts from their url.
+
+For instance, in the following url, http://www.stackprinter.com/export?format=HTML&service=stackoverflow&printer=false&question=31681373, the code you need is the number at the end: '31681373'.
+
+2. Insert the codes into the `codes` list within the script.
+3. Run the script
+    1. From terminal: `python path-to-so2pdf/so2pdf.py`
+    1. From python: `run path-to-so2pdf/so2pdf.py`
+4. It will generate the pdfs into the folder from which you run it.
--- so2pdf.py
+++ so2pdf.py
@@ -3,23 +3,31 @@ from bs4 import BeautifulSoup
 from weasyprint import HTML
 import re
 
-
-codes = [31681373, 9163407, 40077432, 989, 18165213, 46893386]
+# Codes from the article links need to be added here
+codes = [31681373, 9163407, 40077432, 18165213, 46893386]
 
 def main(codes):
+    """Prints stackoverflow posts based on their code.
 
+    1. Takes the code list
+    2. Calls the stackprinter api with the first code
+    3. Parses the print-friendly page
+    4. Prints it into pdf
+    5. Repeats until there are not codes left.
+    """
     if len(codes) > 0:
         url = "http://www.stackprinter.com/export?format=HTML&service=stackoverflow&printer=false&question={}".format(codes[0])
 
-        print(url)
 
         bs = BeautifulSoup(urlopen(url), 'lxml')
         title = bs.title.string.replace('/', ' or ')
 
+        print("\n" + title + "\n" + url)
+
         HTML(url).write_pdf("{}.pdf".format(title))
         return main(codes[1:])
 
-    print("Printed all the pages...")
+    print("\nPrinted all the pages...")
 
 if __name__ == '__main__':
     main(codes)
